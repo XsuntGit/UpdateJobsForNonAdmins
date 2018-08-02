4d01035f4f210ffa089e30a57c9c4a4606fdf14c
Andrey Korneyev <32826269+akorneyev@users.noreply.github.com>
2018-08-02 00:33:56 -0400

    Create Setting_Exec_Rights_to_SQLAgentOperatorRole.sql

---
+++ Setting_Exec_Rights_to_SQLAgentOperatorRole.sql
@@ -0,0 +1,8 @@
+GRANT EXECUTE ON [dbo].[sp_update_job_for_non_admins] TO [SQLAgentOperatorRole]
+GO
+
+GRANT EXECUTE ON [dbo].[sp_update_jobstep_for_non_admins] TO [SQLAgentOperatorRole]
+GO
+
+GRANT EXECUTE ON [dbo].[sp_update_jobschedule_for_non_admins] TO [SQLAgentOperatorRole]
+GO
